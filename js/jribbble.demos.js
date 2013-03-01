87fd5122228e2db132fdeac99d235ac7d658a44a
Tyler Gaw <me@tylergaw.com>
2013-03-01 00:14:16 -0500

    Refreshing examples, fixing some busted styles

--- js/jribbble.demos.js
+++ js/jribbble.demos.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {		
-		
+$(document).ready(function () {
+
 	// API Ref: http://api.dribbble/shots/:id
-	$.jribbble.getShotById(196071, function (shot) {
+	$.jribbble.getShotById(943527, function (shot) {
 		var html = [];
-		
+
 		$('#shotById a:first').attr('href', shot.url);
 		$('#shotById img').attr('src', shot.image_url);
 		$('#shotById h3').text(shot.title);
-		$('#shotById h4').html('by <a href="' + shot.player.url + '">' + 
+		$('#shotById h4').html('by <a href="' + shot.player.url + '">' +
 			shot.player.name + '</a>');
-		
+
 		html.push('<li><b>Views:</b> ' + shot.views_count + '</li>');
 		html.push('<li><b>Likes:</b> ' + shot.likes_count + '</li>');
 		html.push('<li><b>Comments:</b> ' + shot.comments_count + '</li>');
 		html.push('<li><b>Rebounds:</b> ' + shot.rebounds_count + '</li>');
-		
+
 		$('#shotById ul').html(html.join(''));
 	});
-	
+
 	// API Ref: http://api.dribbble/shots/:id/comments
-	$.jribbble.getCommentsOfShot(51986, function (response) {
+	$.jribbble.getCommentsOfShot(953794, function (response) {
 		var html = [];
 		$.each(response.comments, function (i, comment) {
 			html.push('<li>');
@@ -31,18 +31,18 @@ $(document).ready(function () {
 		});
 		$('#shotCommentsList').html(html.join(''));
 	}, {page: 1, per_page: 5});
-	
+
 	// Function used only for comments demo full sized shot
-	$.jribbble.getShotById(51986, function (shot) {
+	$.jribbble.getShotById(953794, function (shot) {
 		$('#shotComments a:first').attr('href', shot.url);
 		$('#shotComments a:first img').attr('src', shot.image_url);
 		$('#shotComments h3').text(shot.title);
-		$('#shotComments h4').html('by <a href="' + shot.player.url + '">' + 
+		$('#shotComments h4').html('by <a href="' + shot.player.url + '">' +
 			shot.player.name + '</a>');
 	});
-	
+
 	// API Ref: http://api.dribbble/shots/:id/rebounds
-	$.jribbble.getReboundsOfShot(10745, function (rebounds) {
+	$.jribbble.getReboundsOfShot(955376, function (rebounds) {
 		var html = [];
 		$.each(rebounds.shots, function (i, shot) {
 			html.push('<li><h3>' + shot.title + '</h3>');
@@ -50,10 +50,10 @@ $(document).ready(function () {
 			html.push('<img src="' + shot.image_teaser_url + '" ');
 			html.push('alt="' + shot.title + '"></a></li>');
 		});
-			
+
 		$('#reboundsOfShot').html(html.join(''));
 	}, {page: 1, per_page: 10});
-	
+
 	// API Ref: http://api.dribbble/shots/:list
 	$.jribbble.getShotsByList('popular', function (listDetails) {
 		var html = [];
@@ -63,12 +63,12 @@ $(document).ready(function () {
 			html.push('<img src="' + shot.image_teaser_url + '" ');
 			html.push('alt="' + shot.title + '"></a></li>');
 		});
-			
+
 		$('#shotsByList').html(html.join(''));
 	}, {page: 2, per_page: 10});
-	
+
 	// API Ref: http://api.dribbble/players/:id/shots
-	$.jribbble.getShotsByPlayerId('tylergaw', function (playerShots) {
+	$.jribbble.getShotsByPlayerId('tykoe', function (playerShots) {
 		var html = [];
 		$.each(playerShots.shots, function (i, shot) {
 			html.push('<li><h3>' + shot.title + '</h3>');
@@ -76,10 +76,10 @@ $(document).ready(function () {
 			html.push('<img src="' + shot.image_teaser_url + '" ');
 			html.push('alt="' + shot.title + '"></a></li>');
 		});
-			
+
 		$('#shotsByPlayerId').html(html.join(''));
 	}, {page: 1, per_page: 10});
-	
+
 	// API Ref: http://api.dribbble/players/:id/shots/following
 	$.jribbble.getShotsThatPlayerFollows('tylergaw', function (followedShots) {
 		var html = [];
@@ -89,10 +89,10 @@ $(document).ready(function () {
 			html.push('<img src="' + shot.image_teaser_url + '" ');
 			html.push('alt="' + shot.title + '"></a></li>');
 		});
-			
+
 		$('#shotsPlayerFollows').html(html.join(''));
 	}, {page: 3, per_page: 10});
-	
+
 	// API Ref: http://api.dribbble/players/:id/shots/likes
 	$.jribbble.getShotsThatPlayerLikes('acreek', function (likedShots) {
 		var html = [];
@@ -102,13 +102,13 @@ $(document).ready(function () {
 			html.push('<img src="' + shot.image_teaser_url + '" ');
 			html.push('alt="' + shot.title + '"></a></li>');
 		});
-			
+
 		$('#shotsPlayerLikes').html(html.join(''));
 	}, {page: 1, per_page: 10});
-	
+
 	// API Ref: http://api.dribbble/players/:id
 	$.jribbble.getPlayerById('tylergaw', function (player) {
-		
+
 		var html = [];
 		html.push('<a href="' + player.url + '"><img src="' + player.avatar_url + '" alt=""></a>');
 		html.push('<div><h3>' + player.name + ' / ' + player.location + '</h3>');
@@ -117,10 +117,10 @@ $(document).ready(function () {
 		html.push('<li><b>Following: </b>' + player.following_count + '</li>');
 		html.push('<li><b>Followers: </b>' + player.followers_count + '</li>');
 		html.push('<li><b>Draftees: </b>' + player.draftees_count + '</li></ul></div>');
-		
+
 		$('#playerProfile').html(html.join(''));
 	});
-	
+
 	// API Ref: http://api.dribbble/players/:id/followers
 	$.jribbble.getPlayerFollowers('robertjpetro', function (followers) {
 		var html = [];
@@ -130,10 +130,10 @@ $(document).ready(function () {
 			html.push('<img src="' + player.avatar_url + '" ');
 			html.push('alt=""></a></li>');
 		});
-		
+
 		$('#playerFollowers').html(html.join(''));
 	}, {per_page: 12});
-	
+
 	// API Ref: http://api.dribbble/players/:id/following
 	$.jribbble.getPlayerFollowing('tylergaw', function (following) {
 		var html = [];
@@ -145,7 +145,7 @@ $(document).ready(function () {
 		});
 		$('#playerFollowing').html(html.join(''));
 	}, {per_page: 12});
-	
+
 	// API Ref: http://api.dribbble/players/:id/draftees
 	$.jribbble.getPlayerDraftees('cameronmoll', function (draftees) {
 		var html = [];
@@ -157,20 +157,20 @@ $(document).ready(function () {
 		});
 		$('#playerDraftees').html(html.join(''));
 	}, {per_page: 12});
-	
-	
+
+
 	// This isn't for the Jribbble demos.
 	// This is a workaround for the Mobile Safari scale orientation bug.
 	// Code from Adactio: http://adactio.com/journal/4470/
 	if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
 		var viewportmeta = document.querySelector('meta[name="viewport"]');
-		
+
 		if (viewportmeta) {
 			viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-			
+
 			document.addEventListener('gesturestart', function () {
 				viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-			}, false);			
+			}, false);
 		}
 	}
 });
